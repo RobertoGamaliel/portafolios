1fb8b0d017574b107f10abcb19789b2bd2c4b7d2
Roberto <gsanchez94@uabc.edu.mx>
2024-02-18 15:09:39 -0800

    prebuild

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portafolios/',
   build: {
     outDir: 'docs',// Carpeta de salida personalizada
   },
