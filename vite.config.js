18eba60fb47cef10e440adc772677e0664dcc0b8
Roberto <gsanchez94@uabc.edu.mx>
2024-02-18 14:49:56 -0800

    main en el root

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Carpeta de salida personalizada
+    outDir: 'docs',// Carpeta de salida personalizada
   },
 })
